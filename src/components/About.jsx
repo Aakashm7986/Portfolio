ce70ad720c2c79de546e0ffc82de92d535866ac0
Aakashm7986 <130494780+Aakashm7986@users.noreply.github.com>
2025-04-18 15:21:51 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Akash, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in ECE, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          Btech, Haldia Institute Of Technology, 3rd Year 
+           
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in C ,C++ , Python,Javascript Experienced with MERN stack Strong grasp of UI/UX
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          As I am in early Stage of mY carrier So at Now I have no  Present Expreince But I Am eager to learn.
+        </span>
+        <br />
+        <br />
+        
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative in MERN stack solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
